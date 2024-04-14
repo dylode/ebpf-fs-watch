7421d2d33de8b23a1e15c4811e77f4c69b5be35d
dylode <spam@geluidtijd.nl>
2024-04-14 14:02:38 +0200

    add bpftool

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <bpf/libbpf.h>
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+    return vfprintf(stderr, format, args);
+}
 
 int main()
 {
+    libbpf_set_print(libbpf_print_fn);
+    libbpf_prog_load();
     printf("hello world");
 }
