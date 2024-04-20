ac353bd0f19364e8ed1fa5b797e3410948ec7a22
dylode <spam@geluidtijd.nl>
2024-04-20 16:17:11 +0200

    getting closer

--- watch.bpf.c
+++ watch.bpf.c
@@ -1,5 +1,6 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
 
 struct exec_params_t
 {
@@ -12,24 +13,20 @@ SEC("tp/syscalls/sys_enter_write")
 int handle_sys_enter_write(struct exec_params_t *ctx)
 {
     // bpf_printk("hi");
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+    struct files_struct *files;
 
-    bpf_printk("write %d", ctx->fd);
+    bpf_core_read(&files, sizeof(void *), &task->files);
+
+    // struct file *fd_array[64];
+    // bpf_core_read(&fd_array, sizeof(fd_array), files->fd_array);
+
+    struct file *file;
+
+    bpf_core_read(&file, sizeof(struct file), files->fd_array[ctx->fd]);
+    //  unsigned char d_iname[40];
+    //  bpf_printk("write %s", file->f_path.dentry->d_iname);
     return 0;
 }
-
-// struct exec_params_t
-//{
-//     u64 __unused;
-//     u64 __unused2;
-//
-//     char *file;
-// };
-//
-// SEC("tp/syscalls/sys_enter_execve")
-// int handle_execve(struct exec_params_t *params)
-//{
-//     bpf_printk("Exec Called %s\n", params->file);
-//     return 0;
-// }
 
 char LICENSE[] SEC("license") = "GPL";
