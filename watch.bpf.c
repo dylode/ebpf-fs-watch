bc79cbc2e13e22a24fa9f01865c0a6161add01d5
dylode <spam@geluidtijd.nl>
2024-05-17 21:45:52 +0200

    read test path

--- watch.bpf.c
+++ watch.bpf.c
@@ -9,6 +9,24 @@ struct exec_params_t
     unsigned int fd;
 };
 
+static bool local_strcmp(const char *cs, const char *ct, int size)
+{
+    int len = 0;
+    unsigned char c1, c2;
+
+#pragma unroll
+    for (len = 0; len < size; len++)
+    {
+        c1 = *cs++;
+        c2 = *ct++;
+
+        if (c1 != c2)
+            return false;
+    }
+
+    return true;
+}
+
 SEC("tp/syscalls/sys_enter_write")
 int handle_sys_enter_write(struct exec_params_t *ctx)
 {
@@ -22,8 +40,54 @@ int handle_sys_enter_write(struct exec_params_t *ctx)
     struct file *f;
     bpf_probe_read(&f, sizeof(f), &fd[ctx->fd]);
 
-    const char *name = BPF_CORE_READ(f, f_path.dentry, d_name.name);
-    bpf_printk("%d => %s", ctx->fd, name);
+    struct dentry *d = BPF_CORE_READ(f, f_path.dentry);
+
+    struct dentry *d_parent;
+    unsigned char *name;
+
+    const char *path[16];
+
+    char buf[64];
+
+    name = BPF_CORE_READ(d, d_name.name);
+    bpf_probe_read_str(buf, sizeof(buf), name);
+
+    d = BPF_CORE_READ(d, d_parent);
+    if (!local_strcmp(buf, "test", 3))
+    {
+        return 0;
+    }
+
+    bpf_printk("%s", name);
+
+    name = BPF_CORE_READ(d, d_name.name);
+    d = BPF_CORE_READ(d, d_parent);
+
+    bpf_printk("%s", name);
+
+    name = BPF_CORE_READ(d, d_name.name);
+    d = BPF_CORE_READ(d, d_parent);
+
+    bpf_printk("%s", name);
+
+    name = BPF_CORE_READ(d, d_name.name);
+    d = BPF_CORE_READ(d, d_parent);
+
+    bpf_printk("%s", name);
+
+    // bpf_repeat(16)
+    //{
+    //     name = BPF_CORE_READ(d, d_name.name);
+    //     bpf_printk("%d => %s", ctx->fd, name);
+    //     d_parent = BPF_CORE_READ(d, d_parent);
+
+    //    if (d_parent == d)
+    //    {
+    //        break;
+    //    }
+
+    //    d = d_parent;
+    //}
 
     //  bpf_probe_read(&d, sizeof(d), f.f_path.dentry);
 
@@ -34,10 +98,6 @@ int handle_sys_enter_write(struct exec_params_t *ctx)
     // }
 
     return 0;
-}
-
-static append_parent_name(struct parent_name *ctx)
-{
 }
 
 char LICENSE[] SEC("license") = "GPL";
